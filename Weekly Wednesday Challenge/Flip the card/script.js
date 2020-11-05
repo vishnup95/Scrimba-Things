45dc5afe8985475872298a32c6dbe1da38bc807a
vishnup95 <vishnu.wish7@gmail.com>
2020-11-05 23:39:46 +0530

    Flip card update

---
+++ Weekly Wednesday Challenge/Flip the card/script.js
@@ -0,0 +1,18 @@
+/*
+
+STRETCH GOALS:
+
+1) Go all out with styles: fonts, sizes, backgrounds, transitions, colours! 
+
+2) Find a purpose for your card: business card, invitation to a party, email subscription, 
+a video reveal or a music album cover with a song player at the back. 
+
+3) Depending on you purpose add some the relevant interactivity: 
+    - business card: links to your social media
+    - invitation to a party, email subscription: confirmation of the subscription 
+    - a video reveal/song card: start playing the video/song
+
+*/
+
+
+
